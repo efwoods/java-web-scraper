e46156a4359adbadd0177177ad35e46f2f1de169
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-28 21:01:30 -0400

    commit

---
+++ src/main/java/com/example/demo/JavaWebScraperApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebScraperApplication.class, args);
+	}
+
+}
