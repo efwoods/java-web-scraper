c71996ab6a5f3f6ea5dce4966cbd211458255d66
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-28 21:27:54 -0400

    commit

--- src/main/java/com/example/demo/JavaWebScraperApplication.java
+++ src/main/java/com/example/demo/JavaWebScraperApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class JavaWebScraperApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavaWebScraperApplication.class, args);
 	}
